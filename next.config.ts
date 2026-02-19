340605d5c9e76c8b8bd9c2c507cb3b81cb25be3f
Wolney Oliveira <wolney.js@outlook.com>
2026-02-19 00:50:36 -0300

    feat: add redirect from root to login page

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-};
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
